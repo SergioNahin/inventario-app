9000a2111d472fb6755148225ede5ddcfcdea2aa
Sergio_Nahin <sergiontr280@gmail.com>
2024-07-25 02:48:20 -0600

    Create listado de servicio con clase

--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Producto  } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html',
   styleUrl: './producto-lista.component.css'
 })
 export class ProductoListaComponent {
+  productos: Producto[];
 
+  constructor(private productoServicio: ProductoService){}
+
+  ngOnInit(){
+    //Carga Productos
+    this.obtenerProductos();
+  }
+   private obtenerProductos(){
+    //Consumir los datos del observable
+    this.productoServicio.obtenerProductosLista().subscribe(
+      datos => {
+        this.productos = datos;
+      }
+    )
+   }
 }
