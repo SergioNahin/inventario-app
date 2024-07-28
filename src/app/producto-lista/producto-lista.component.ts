4efb734ce09799da039b9471db733dcdace9e3e5
Sergio_Nahin <sergiontr280@gmail.com>
2024-07-27 21:54:56 -0600

    Add function obtenerid for editing and add settings.json for language Spanish

--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
-import { Producto  } from '../producto';
+import { Producto } from '../producto';
 import { ProductoService } from '../producto.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html',
-  styleUrl: './producto-lista.component.css'
+  styleUrl: './producto-lista.component.css',
 })
 export class ProductoListaComponent {
   productos: Producto[];
 
-  constructor(private productoServicio: ProductoService){}
+  constructor(private productoServicio: ProductoService,
+    private enrutador: Router) {}
 
-  ngOnInit(){
+  ngOnInit() {
     //Carga Productos
     this.obtenerProductos();
   }
-   private obtenerProductos(){
+  private obtenerProductos() {
     //Consumir los datos del observable
-    this.productoServicio.obtenerProductosLista().subscribe(
-      datos => {
-        this.productos = datos;
-      }
-    )
-   }
+    this.productoServicio.obtenerProductosLista().subscribe((datos) => {
+      this.productos = datos;
+    });
+  }
+  editarProducto(id: number) {
+    this.enrutador.navigate(['editar-producto',id])
+  }
 }
